687619e8d3bc8e7f0394509f7c1317d570513463
amtisyAts <2835908979@qq.com>
2022-07-18 11:40:42 +0800

    2334

--- lib/home/hot.dart
+++ lib/home/hot.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'question_page.dart';
 
 class Hot extends StatefulWidget{
   @override
   _HotState createState() => new _HotState();
 }
 class _HotState extends State<Hot>{
+
+  Widget hotCard(Question question){
+    var flatButtonOnPressed = (){
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context){
+            return new QuestionPage();}));
+    };
+    var flatButtonChild = new Container(
+
+    );
+    return new Container(
+      child: new FlatButton(
+          onPressed: flatButtonOnPressed,
+          child: flatButtonChild)
+    );
+  }
+
   @override
   Widget build(BuildContext context){
-    return new SingleChildScrollView();
+    return new SingleChildScrollView(
+      child: new Container(
+        child: new Column(
+          children: <Widget>[
+            new Container(
+
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/home/question_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestionPage extends StatefulWidget{
+  @override
+  _QuestionPageState createState() => new _QuestionPageState();
+}
+class _QuestionPageState extends State<QuestionPage>{
+  @override
+  Widget build(BuildContext context){
+    return new SingleChildScrollView();
+  }
+}
