549228c11ab72b5aa851d6ebc8402ce4bd5e68f9
amtisyAts <2835908979@qq.com>
2022-07-27 07:18:24 +0800

    add search hot list

--- lib/home/search.dart
+++ lib/home/search.dart
@@ -18,6 +18,16 @@ class _SearchState extends State<Search>{
         });
       }
     });
+    var searchHotList = [
+      "油价或将三连跌",
+      "专家：预售制...",
+      "乐山2名儿童检...",
+      "2022f1法国大奖赛",
+      "抖音无脑反夏日...",
+      "阿里双重上市",
+      "广州12345回应",
+      "赵露思吴磊"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -46,7 +56,7 @@ class _SearchState extends State<Search>{
             children: [
               Row(
                 children: [
-                  Text("                                           历史搜索",style: TextStyle(fontSize: 15,color: Colors.black),),Spacer(),
+                  Text("  历史搜索",style: TextStyle(fontSize: 15,color: Colors.black),),Spacer(),
                   IconButton(onPressed: ()=>{
                     setState(() {
                       historySearchList = [];
@@ -59,7 +69,21 @@ class _SearchState extends State<Search>{
                 children:[
                 ...historySearchList.map((e) => TextButton(onPressed: ()=>{
                 print("请求搜索历史搜索过的问题：${e}")
-                }, child: Text(e,style: TextStyle(fontSize: 15,color: Colors.black),))).toList(),]
+                }, child: Text(e,textAlign: TextAlign.left,style: TextStyle(fontSize: 15,color: Colors.black),))).toList(),]
+              ),
+              Row(
+                children: [Text("  搜索发现",style: TextStyle(fontSize: 15)),Spacer(),TextButton.icon(icon: Icon(Icons.recycling,color: Colors.black26,),label: Text("换一换",style: TextStyle(fontSize: 15,color: Colors.black26),),onPressed: ()=>{print("刷新搜索热点")},)],
+              ),
+              Container(
+                height: 300,
+                width: 300,
+                child: GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 2 //宽高比为1时，子widget
+                    ),
+                  children: searchHotList.map((e) => Text("${e}",style: TextStyle(color: Colors.black,fontSize :15))).toList(),
+                ),
               )
             ],
           )
